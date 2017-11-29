8df884469bfefdb579cbf4671f3ad46d6d7c3d2d
Kacppian <aspkaushik@gmail.com>
2017-11-29 13:34:55 +0530

    Removed useless code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-import Search from "./js/Searchbar";
 import './sass/Searchbar.sass';
 
 ReactDOM.render(
 	<div>
 		<h1>Hello, React!</h1>
-		<Search value='Default'/>
 	</div>,
 	document.getElementById('root')	
 );
